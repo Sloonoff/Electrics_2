b17dd40b0b6e0e72a7a26a848c07c85bc84844f2
Sloonoff <131548569+Sloonoff@users.noreply.github.com>
2023-06-15 22:42:19 +0400

    Update keyboard_test.py

--- tests/keyboard_test.py
+++ tests/keyboard_test.py
@@ -1,3 +1,39 @@
+from src.keyboard import Keyboard
+
+def test_keyboard_creation():
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
+    assert str(kb) == "Dark Project KD87A"
+
+def test_keyboard_language():
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
+    assert str(kb.language) == "EN"
+
+def test_keyboard_change_lang_invalid():
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
+    try:
+        kb.language = 'CH'
+    except AttributeError as e:
+        assert str(e) == "can't set attribute"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # from src.keyboard import Keyboard
 #
 # def test_keyboard_creation():
