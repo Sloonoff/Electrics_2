9af0e03cca4e5db1935283197dddf3568520b32b
Sloonoff <131548569+Sloonoff@users.noreply.github.com>
2023-06-15 22:41:54 +0400

    Update test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,29 +1,83 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""  # Дз_4
-from src.item import Item
+"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+import csv
+from src.item import Item, InstantiateCSVError
+
+@pytest.fixture
+def items_csv(tmp_path):
+    data = [("apple", 0.5, 10), ("banana", 0.4, 15), ("pear", 0.6, 7)]
+    filepath = tmp_path / "items.csv"
+    with open(filepath, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
+    return filepath
+
+def test_instantiate_from_csv(items_csv):
+    Item.instantiate_from_csv(items_csv)
+    assert len(Item.all) == 3
+    assert Item.all[0].name == "apple"
+    assert Item.all[0].price == 0.5
+    assert Item.all[0].quantity == 10
+    assert Item.all[1].name == "banana"
+    assert Item.all[1].price == 0.4
+    assert Item.all[1].quantity == 15
+    assert Item.all[2].name == "pear"
+    assert Item.all[2].price == 0.6
+    assert Item.all[2].quantity == 7
+
+def test_instantiate_from_csv_file_not_found(tmp_path):
+    with pytest.raises(FileNotFoundError):
+        Item.instantiate_from_csv(tmp_path / "nonexistent.csv")
 
 
-def test_item_init():
-    item = Item("Телефон", 10_000, 20)
-    assert item.name == "Телефон"
-    assert item.price == 10_000
-    assert item.quantity == 20
 
 
-def test_item_add():
-    item1 = Item("Телефон", 10_000, 20)
-    item2 = Item("Наушники", 5_000, 30)
-    result = item1.add(item2)
-    assert result.quantity == item1.quantity + item2.quantity
 
 
-def test_item_str():
-    item = Item("Телефон", 10_000, 20)
-    assert str(item) == "Телефон"
 
 
-def test_item_repr():
-    item = Item("Телефон", 10_000, 20)
-    assert repr(item) == "Item('Телефон', 10000, 20)"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# """Здесь надо написать тесты с использованием pytest для модуля item."""  # Дз_4
+# from src.item import Item
+
+
+# def test_item_init():
+#     item = Item("Телефон", 10_000, 20)
+#     assert item.name == "Телефон"
+#     assert item.price == 10_000
+#     assert item.quantity == 20
+
+
+# def test_item_add():
+#     item1 = Item("Телефон", 10_000, 20)
+#     item2 = Item("Наушники", 5_000, 30)
+#     result = item1.add(item2)
+#     assert result.quantity == item1.quantity + item2.quantity
+
+
+# def test_item_str():
+#     item = Item("Телефон", 10_000, 20)
+#     assert str(item) == "Телефон"
+
+
+# def test_item_repr():
+#     item = Item("Телефон", 10_000, 20)
+#     assert repr(item) == "Item('Телефон', 10000, 20)"
 
 
 
